35db74515e60cd12e037dcb7aca5d1c18e84629a
root <root@localhost.localdomain>
2019-02-04 21:12:27 +0530

    edited

--- module_map/src/main/java/everest/ModuleMap.java
+++ module_map/src/main/java/everest/ModuleMap.java
@@ -1,14 +1,14 @@
 class ModuleMap
 {
-	int a=20;//instance
-	static int a=30;//static 
+	//int a=20;//instance
+	//static int a=30;//static 
 	static double b=100.89;
 	void m1()
 	{
-		int a=40;//local
+		//int a=40;//local
 		double b=111.11;
-		System.out.println(a);
-		System.out.println(a);
+		//System.out.println(a);
+		//System.out.println(a);
 		System.out.println(b);
 	}
 
